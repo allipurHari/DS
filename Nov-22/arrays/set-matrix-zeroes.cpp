4b6e568df0ffcb9ae682eccfdccac373b4881111
allipurHari <37536117+allipurHari@users.noreply.github.com>
2022-11-28 23:25:01 +0530

    Create set-matrix-zeroes.cpp

---
+++ Nov-22/arrays/set-matrix-zeroes.cpp
@@ -0,0 +1,30 @@
+https://leetcode.com/problems/set-matrix-zeroes/submissions/
+
+// Silly declaration mistakes, it felt like I'm in a hurry to execute the code only concerning about the core logic
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& m) {
+        int rs = m.size();
+        if(!rs)
+            return;
+        int cs = m[0].size();
+        vector<int>r(rs, 0), c(cs, 0);
+        for(int i = 0; i < rs;i++){
+            for(int j = 0;j < cs;j++){
+                if(m[i][j] == 0){
+                    r[i] = 1;
+                    c[j] = 1;
+                }
+            }
+        }
+        
+        for(int i = 0; i < rs;i++){
+            for(int j = 0;j < cs;j++){
+                if(r[i] == 1 || c[j] == 1){
+                    m[i][j] = 0;
+                }
+            }
+        }
+    }
+};
