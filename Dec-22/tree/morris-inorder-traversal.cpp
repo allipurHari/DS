cb7476e964b5a326900d6d9553b7c5b3c2869648
allipurHari <37536117+allipurHari@users.noreply.github.com>
2022-12-13 19:34:08 +0530

    Create morris-inorder-traversal.cpp

---
+++ Dec-22/tree/morris-inorder-traversal.cpp
@@ -0,0 +1,44 @@
+https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/859182256/
+https://www.educative.io/answers/what-is-morris-traversal
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>ans;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(!root)return ans;
+        TreeNode *curr = root, *prev;
+        while(curr){
+            if(curr->left == nullptr){
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                prev = curr->left;
+                while(prev->right && prev->right!= curr){
+                    prev = prev->right;
+                }
+                if(prev->right == nullptr){
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    prev->right = nullptr;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
