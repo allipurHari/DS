78b4cc4cdb7c846f604a7810fc3ccca064e1ae56
allipurHari <37536117+allipurHari@users.noreply.github.com>
2022-12-29 19:36:35 +0530

    Update subsets-2.cpp

--- Dec-22/backtracking/subsets-2.cpp
+++ Dec-22/backtracking/subsets-2.cpp
@@ -10,6 +10,34 @@ public:
         solve(nums, v, idx+1, s);
         v.pop_back();
     }
+    
+    // Iterative
+        vector<vector<int>> solve(vector<int> nums){
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans = {{}};
+        if(!nums.size())return ans;
+        ans.push_back(vector<int>(1, nums[0]));
+        int startIdx = 1;
+        for(int i = 1;i < nums.size();i++){
+            int j = 0;
+            if(nums[i] == nums[i-1]){
+                j = startIdx;
+            }
+            if(i == nums.size())
+                break;
+            int s = ans.size();
+            int count = 0;
+            for(;j < s;j++){
+                vector<int>temp = ans[j];
+                temp.push_back(nums[i]);
+                ans.push_back(temp);
+                count++;
+            }
+            startIdx = ans.size()-count;
+        }
+        return ans;
+    }
+    
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         set<vector<int>> s;
         vector<int>v;
